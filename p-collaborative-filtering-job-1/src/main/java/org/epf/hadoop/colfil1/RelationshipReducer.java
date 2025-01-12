71cddcefbb2a07725bc207ba0a3690fa1db44a3b
ewarskotte <elea.warskotte@epfedu.fr>
2025-01-12 17:56:14 +0100

    refraction des bugs

--- p-collaborative-filtering-job-1/src/main/java/org/epf/hadoop/colfil1/RelationshipReducer.java
+++ p-collaborative-filtering-job-1/src/main/java/org/epf/hadoop/colfil1/RelationshipReducer.java
@@ -4,15 +4,23 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringJoiner;
 
 public class RelationshipReducer extends Reducer<Text, Text, Text, Text> {
+
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        StringJoiner joiner = new StringJoiner(",");
+        Set<String> uniqueRelationships = new HashSet<>();
         for (Text val : values) {
-            joiner.add(val.toString());
+            uniqueRelationships.add(val.toString());
         }
+        StringJoiner joiner = new StringJoiner(",");
+        uniqueRelationships.stream()
+                .sorted()
+                .forEach(joiner::add);
         context.write(key, new Text(joiner.toString()));
     }
 }
+
